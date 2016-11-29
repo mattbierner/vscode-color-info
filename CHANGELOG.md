c6f6ad7e83fc1c06d71b8889e469e3d9fe1e6b9f
Matt Bierner <mattbierner@gmail.com>
2016-11-28 20:33:46 -0800

    Fix alpha display

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1 – Nov 28, 2016
+* Fix alpha in preview display not showing.
+
+
 ## 0.1.0 – Nov 28, 2016
 * Added color preview displays.
 * Made settings use enum values for better intellisense.
--- src/displays/preview_display.ts
+++ src/displays/preview_display.ts
@@ -24,8 +24,8 @@ class PreviewDisplay implements ColorInfoDisplay {
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" style="background-color: white;"
 	 width="${this._width}px" height="${this._height}px" viewBox="0 0 ${this._width} ${this._height}" xml:space="preserve">
-        <rect width="${this._width}" height="${this._height}" fill="${hex}" />
-        <polygon points="0,0 0,${this._height} ${this._width},${this._height}" fill="${hex}" fill-opacity="${match.color.getAlpha()}" />
+        <rect width="${this._width}" height="${this._height}" fill="${hex}" fill-opacity="${match.color.getAlpha()}" />
+        <polygon points="0,0 ${this._width},0 ${this._width},${this._height}" fill="${hex}" />
         <rect width="${this._width}" height="${this._height}" fill-opacity="0" stroke="gray" strokeWidth="1" />
 </svg>`;
         datauri.format('.svg', src);
